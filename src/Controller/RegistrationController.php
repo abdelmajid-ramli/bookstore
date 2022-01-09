07d4c478a97ed460fec47b41b3d9a3a556126f04
abdelmajid-ramli <abdalmajidardesigns@gmail.com>
2022-01-09 14:38:50 +0100

    controller done

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,6 +18,11 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('livre_index');
+        }
+        
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
